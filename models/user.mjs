4886ae8bbff42a72fec42b07849b34ffe5433120
Daniel <danieladeyd77@gmail.com>
2025-08-03 04:16:43 +0100

    server running and databse connectedt

--- models/user.mjs
+++ models/user.mjs
@@ -1,5 +1,29 @@
+import { timeStamp } from "console";
 import mongoose from "mongoose";
 
-const UserScheme = mongoose.Schema({
-    
-});
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+
+    role: {
+        type: String,
+        enum: [`user`, `admin`],  //there is a user access and an admin access 
+    }
+}, {timeStamps : true});
