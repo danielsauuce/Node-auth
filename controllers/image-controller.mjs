7361de81edbd4b2c59c5327a46563dbfd0153431
Daniel <danieladeyd77@gmail.com>
2025-08-21 14:36:11 +0100

    sorting and pagination added to files upload

--- controllers/image-controller.mjs
+++ controllers/image-controller.mjs
@@ -39,11 +39,26 @@ const uploadImageController = async (req, res) => {
 
 const fetchImagesController = async (req, res) => {
   try {
-    const images = await Image.find({});
+    const page = parseInt(req.query.page) || 1; //default page to start from 
+    const limit = parseInt(req.query.limit) || 5; //amount of image i want to see per page
+    const skip = (page - 1) * limit; //skip already displayed images and move to the next
+
+    const sortBy = req.query.sortBy || "createdAt";
+    const sortOrder = req.query.sortOrder === "asc" ? 1 : -1;
+    const totalImages = await Image.countDocuments();
+    const totalPages = Math.ceil(totalImages / limit);
+
+    const sortObj = {};
+    sortObj[sortBy] = sortOrder;
+
+    const images = await Image.find().sort(sortObj).skip(skip).limit(limit);
 
     if (images) {
       res.status(200).json({
         success: true,
+        currentPage: page,
+        totalPages: totalPages,
+        totalImages: totalImages,
         data: images,
       });
     }
